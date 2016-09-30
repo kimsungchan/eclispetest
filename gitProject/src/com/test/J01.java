37d80ba6dde7cf1a31a3a81e4ddef484f6a0998b
김성찬 <dlrj1234@naver.com>
2016-09-30 19:29:51 +0900

    create project

---
+++ gitProject/src/com/test/J01.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class J01 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello");
+
+	}
+
+}
