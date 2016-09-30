7926299d791c4a948ee5128319204fb9ea3952cc
김성찬 <dlrj1234@naver.com>
2016-09-30 19:38:02 +0900

    fix j01 -- hello수정

--- gitProject/src/com/test/J01.java
+++ gitProject/src/com/test/J01.java
@@ -7,7 +7,7 @@ public class J01 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Hello");
-		System.out.println("Hello");
+		System.out.println("FixJ01");
 
 	}
 
