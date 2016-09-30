41b954d57b9128886a78856a9a8328c69da65669
김성찬 <dlrj1234@naver.com>
2016-09-30 19:54:21 +0900

    5주차 1번 수정중

--- gitProject/src/com/test/J01.java
+++ gitProject/src/com/test/J01.java
@@ -1,14 +1,25 @@
 package com.test;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class J01 {
-
+    int num1,num2,num3;
+    int max_num, min_num;
+    
+    void input(){
+        Scanner s = new Scanner(System.in);
+    	System.out.print("숫자를 입력하세요 :");
+    	int first = s.nextInt();
+    	System.out.print("숫자를 입력하세요 :");
+    	int second = s.nextInt();
+    	System.out.print("숫자를 입력하세요 :");
+    	int third = s.nextInt();
+    	
+    }
+   
+	
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Hello");
-		System.out.println("FixJ01");
-
+		
 	}
 
 }
